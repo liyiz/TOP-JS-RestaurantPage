63d7c4afcea9ed3390643042487140e637302510
liyiz <5160472+liyiz@users.noreply.github.com>
2025-04-14 11:23:57 -0400

    Add test image bundling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles.css";
 import { testmsg } from "./test.js";
+import odinImage from "./odin.jpg"
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
 
 console.log(testmsg);
