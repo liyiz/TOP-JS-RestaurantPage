ccf0699971a1a38a291cba96ad4ef33b5958f6b4
liyiz <5160472+liyiz@users.noreply.github.com>
2025-04-15 10:50:50 -0400

    Initial layout styling

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import cantineImage from "./img/cantine.jpg"
+
 const renderHome = (container) => {
 
     const home = document.createElement('div');
@@ -6,8 +8,16 @@ const renderHome = (container) => {
     const heading = document.createElement('h1');
     heading.textContent = "Cantine O' Canada";
 
+    const img_cantine = document.createElement("img");
+    img_cantine.src = cantineImage;
+    img_cantine.classList.add('hero');
+
     home.appendChild(heading);
+    home.appendChild(img_cantine);
+
     container.appendChild(home);
+
+
 }
 
 export { renderHome }
