6dfa13735c9716d27068de48ceb05332c90f536d
liyiz <5160472+liyiz@users.noreply.github.com>
2025-04-28 18:33:44 -0400

    improved menu item card layout

--- src/menu.js
+++ src/menu.js
@@ -15,30 +15,24 @@ const renderMenu = (container) => {
     const item = document.createElement('div');
     item.classList.add('menu-item');
 
-    const item_listing = document.createElement('div');
-    item_listing.classList.add('menu-item-listing');
-
-    const item_blurb = document.createElement('p');
+    const item_blurb = document.createElement('div');
     item_blurb.classList.add('menu-item-blurb');
     
     const item_name = document.createElement('p');
     item_name.textContent = 'menu item #01'
     const item_description = document.createElement('p');
-    item_description.textContent = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.'
+    item_description.textContent = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.'
+    const item_price = document.createElement('p');
+    item_price.textContent = '$14.99'
 
     item_blurb.appendChild(item_name);
     item_blurb.appendChild(item_description);
-
-    const item_price = document.createElement('p');
-    item_price.textContent = '$4.99'
-
-    item_listing.appendChild(item_blurb);
-    item_listing.appendChild(item_price);
+    item_blurb.appendChild(item_price);
 
     const item_img = document.createElement('img');
     item_img.src = 'https://picsum.photos/200';
 
-    item.appendChild(item_listing);
+    item.appendChild(item_blurb);
     item.appendChild(item_img);
 
     menu.appendChild(heading);
