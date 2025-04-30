25115dcc76bbf044403a75298e5aaa94f1a3da5f
liyiz <5160472+liyiz@users.noreply.github.com>
2025-04-30 14:26:31 -0400

    Create renderMenuItem function

--- src/menu.js
+++ src/menu.js
@@ -12,28 +12,12 @@ const renderMenu = (container) => {
     img_menu.src = menuImage;
     img_menu.classList.add('hero');
 
-    const item = document.createElement('div');
-    item.classList.add('menu-item');
-
-    const item_blurb = document.createElement('div');
-    item_blurb.classList.add('menu-item-blurb');
-    
-    const item_name = document.createElement('p');
-    item_name.textContent = 'menu item #01'
-    const item_description = document.createElement('p');
-    item_description.textContent = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.'
-    const item_price = document.createElement('p');
-    item_price.textContent = '$14.99'
-
-    item_blurb.appendChild(item_name);
-    item_blurb.appendChild(item_description);
-    item_blurb.appendChild(item_price);
-
-    const item_img = document.createElement('img');
-    item_img.src = 'https://picsum.photos/200';
-
-    item.appendChild(item_blurb);
-    item.appendChild(item_img);
+    const item = renderMenuItem(
+        'Delicious Chicken Wings', 
+        'A dozen bbq sauce covered chicken wings, grilled over charcoal flames and comes with blue ranch sauce and a garnish of celery sticks.',
+        '$14.99',
+        'https://picsum.photos/200'
+    );
 
     menu.appendChild(heading);
     menu.appendChild(img_menu);
@@ -45,6 +29,35 @@ const renderMenu = (container) => {
     // TODO:
     // 1. Create objects array of menu items - name, description, price, image
     // 2. Create a function that takes objects array and then populates the menu cards.
+
+}
+
+const renderMenuItem = (name, description, price, img_src) => {
+
+    const item = document.createElement('div');
+    item.classList.add('menu-item');
+
+    const item_blurb = document.createElement('div');
+    item_blurb.classList.add('menu-item-blurb');
+    
+    const item_name = document.createElement('p');
+    item_name.textContent = name;
+    const item_description = document.createElement('p');
+    item_description.textContent = description;
+    const item_price = document.createElement('p');
+    item_price.textContent = price;
+
+    item_blurb.appendChild(item_name);
+    item_blurb.appendChild(item_description);
+    item_blurb.appendChild(item_price);
+
+    const item_img = document.createElement('img');
+    item_img.src = img_src;
+
+    item.appendChild(item_blurb);
+    item.appendChild(item_img);
+
+    return item;
 
 }
 
