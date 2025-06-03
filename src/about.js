4d90e9e2f64ab536e361fbf5037649db00d5ed15
liyiz <5160472+liyiz@users.noreply.github.com>
2025-06-03 11:13:23 -0400

    Add about text

--- src/about.js
+++ src/about.js
@@ -8,12 +8,17 @@ const renderAbout = (container) => {
     const heading = document.createElement('h1');
     heading.textContent = "About";
 
-    const img_about = document.createElement("img");
+    const img_about = document.createElement('img');
     img_about.src = aboutImage;
     img_about.classList.add('hero');
 
+    const about_body = document.createElement('p');
+    about_body.textContent = "Never heard of CHICKEN WINGS? No problem, we're here to help your life taste better. We welcome you to the Wingers Culture of 'Winging it', one of the best stomach fillers to ever be invented! Get your box of CRISPY and JUICY chicken wings with our special recipe sauces! ALL OUR CHICKEN IS HALAL! Our Kitchen Closes an hour before our official closing times!"
+
+    
     about.appendChild(heading);
     about.appendChild(img_about);
+    about.appendChild(about_body);
     container.appendChild(about);
 }
 
